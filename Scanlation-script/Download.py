66e4c1f19752433499621266c8d5ebfafbe4a8d7
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2022-08-13 23:11:23 +0200

    Add files via upload

---
+++ Scanlation-script/Download.py
@@ -0,0 +1,36 @@
+from requests import get
+from re import findall
+from sys import argv
+from os import makedirs, chdir
+
+if len(argv) > 1:
+    result = argv[1]
+else:
+    result = False
+
+if result == False:
+    print("Usage: python3 download.py <chapter>")
+    exit()
+else:
+    pass
+url = "https://mangaesp.co/ver/la-piba-del-tinder/" + argv[1]
+
+
+r = get(url)
+
+links = findall("https://imagizer.imageshack.com.*.jpg", r.text)
+if len(links) < 5:
+        links = findall("https://i.yapx.ru.*.jpg", r.text)
+
+if len(links) < 5:
+        links = findall("http://i.yapx.ru.*.jpg", r.text)
+
+makedirs(argv[1])
+chdir(argv[1])
+
+for link,name in zip(links, range(len(links))):
+    r = get(link)
+    print(f"Downloading Picture: {str(name)}.jpg")
+    open(str(name) + ".jpg", "wb").write(r.content)
+
+print("Done!")
