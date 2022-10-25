edc4e79b9b3af6ac3ede1844d9f93ada273e9ea1
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2022-10-25 19:57:15 +0200

    Update Download.py

--- Scanlation-script/Download.py
+++ Scanlation-script/Download.py
@@ -1,7 +1,14 @@
 from requests import get
 from re import findall
 from sys import argv
-from os import makedirs, chdir
+from os import makedirs, chdir, remove
+from PIL import Image
+from queue import Queue
+from threading import Thread
+
+import datetime
+
+start_time = datetime.datetime.now()
 
 if len(argv) > 1:
     result = argv[1]
@@ -13,24 +20,56 @@ if result == False:
     exit()
 else:
     pass
-url = "https://mangaesp.co/ver/the-girl-from-tinder/" + argv[1]
+url = "https://mangaesp.co/ver/junwoo-wonka-y-la-fabrica-de-prras-locas/" + argv[1]
 
-
+q = Queue()
 r = get(url)
+names = Queue()
 
 links = findall("https://imagizer.imageshack.com.*.jpg", r.text)
-if len(links) < 5:
+if len(links) < 1:
         links = findall("https://i.yapx.ru.*.jpg", r.text)
 
-if len(links) < 5:
+if len(links) < 1:
         links = findall("http://i.yapx.ru.*.jpg", r.text)
+
+if len(links) < 1:
+        links = findall("https://cdn.snf18.xyz/images/.*.webp", r.text)
+
+for url in links:
+    q.put(url)
+for name in range(len(links)):
+    names.put(name)
 
 makedirs(argv[1])
 chdir(argv[1])
 
-for link,name in zip(links, range(len(links))):
-    r = get(link)
-    print(f"Downloading Picture: {str(name)}.jpg")
-    open(str(name) + ".jpg", "wb").write(r.content)
+def downloadlink():
+    while not q.empty():
+        link = q.get()
+        name = names.get()
+        r = get(link)
+        if link.endswith("webp"):
+            print(f"Downloading Picture: {str(name)}.jpg")
+            open(str(name) + ".webp", "wb").write(r.content)
+            Image.open(str(name) + ".webp").save(str(name) + ".jpg", "jpeg")
+            remove(str(name) + ".webp")
+
+        elif link.endswith("jpg"):
+            print(f"Downloading Picture: {str(name)}.jpg")
+            open(str(name) + ".jpg", "wb").write(r.content)
+        q.task_done()
+
+
+def download_all():
+    for i in range(len(links)):
+        t_worker = Thread(target=downloadlink)
+        t_worker.start()
+    q.join()
+
+download_all()
 
 print("Done!")
+
+end_time = datetime.datetime.now()
+print(end_time - start_time)
