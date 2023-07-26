c947a266a7423d2ca205942112e8ac9aab6a7c18
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2023-07-27 00:31:42 +0200

    Update Anichecker.py

--- Anichecker/Anichecker.py
+++ Anichecker/Anichecker.py
@@ -2,6 +2,7 @@ import sqlite3
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service 
 from webdriver_manager.chrome import ChromeDriverManager
 import os
 
@@ -17,20 +18,18 @@ except:
 conn.commit()
 
 if os.name == "nt":
-	desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
+	file_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') + "\\Updated.txt".replace("\\", "\\")
 else:
-	desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop') 
+	file_path = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop') + "/Updated.txt"
 
 options = ChromeOptions()
-options.headless = True
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_argument("--headless=new")
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 urls = open(dir_path + "/" "urls.txt").read().split()
-file_path = f"{desktop}\\Updated.txt".replace("\\", "\\")
 
-for url in urls:
+for url in urls:	
 	r = driver.get(url)
 	soup = BeautifulSoup(driver.page_source, 'html.parser')
 	name = soup.find(class_="film-name dynamic-name").text
@@ -46,14 +45,12 @@ for url in urls:
 		print(f"{name} Added to the DB")
 		continue
 	if len(eps) > previous_ep:
-		open(file_path, "a").write(f"{name} Ep {len(eps)} https://9animetv.to{new_ep}\n")
+		open(file_path, "a").write(f"{name}  Ep {len(eps)}  https://9animetv.to{new_ep}\n")
 		c.execute('UPDATE an SET ep = {} WHERE url = "{}"'.format(len(eps), url))
 		conn.commit()
 		print(f"{name} Updated to {eps}")
 	else:
 		pass
-	conn.commit()
 
-
-conn.close()
 driver.close()
+conn.close()
