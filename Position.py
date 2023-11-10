7d0dc8fede766c70d8fdb85411d5b04e50501ba2
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2023-11-10 20:43:43 +0100

    Create Position.py

---
+++ Position.py
@@ -0,0 +1,12 @@
+from pynput import mouse
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        if button == mouse.Button.left:
+            print('{} at {}'.format('Pressed Left Click', (x, y)))
+        else:
+            print('{} at {}'.format('Pressed Right Click', (x, y)))
+
+listener = mouse.Listener(on_click=on_click)
+listener.start()
+listener.join()
