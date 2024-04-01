7c2bff7064bd6e7d1123f1fb459157ba3de18629
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2024-04-01 02:07:05 +0200

    Add files via upload

---
+++ Scripts/sub_remover.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import subprocess
+import os
+
+path = os.getcwd()
+files = os.listdir(path)
+files =  [file for file in files if file.endswith(".mkv")]
+
+options = {}
+
+for file in files:
+    mkvmerge_output = subprocess.run(["mkvmerge", "-i", os.path.join(path, file)], capture_output=True, text=True)
+
+    for line in mkvmerge_output.stdout.splitlines():
+        if "subtitles" in line and line not in options:
+            track_id = line.split()[2].strip(":")
+            options[track_id] = line
+
+print("\n".join(options.values()))
+
+choice = input("\nWhat Track ID to use: ")
+
+while choice not in options:
+    choice = input("\nSorry not a valid ID: ")
+
+for file in files:
+    mkvmerge_output = subprocess.run(["mkvmerge" , "-o", f"{os.path.join(path, file)}CACHE", "-s", choice, f"{os.path.join(path, file)}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["mv", f"{os.path.join(path, file)}CACHE", os.path.join(path, file)])  # Fixed this line
+    print(f"✅ {file} done")
