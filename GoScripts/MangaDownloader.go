56dd1a5ec956d568727f5f31e3391debe73c6360
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2024-05-24 01:59:11 +0200

    Update MangaDownloader.go

--- GoScripts/MangaDownloader.go
+++ GoScripts/MangaDownloader.go
@@ -8,6 +8,8 @@ import (
 	"path"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -44,51 +46,114 @@ func main() {
 
 	manga_path := path.Join(cwd, name)
 	os.Mkdir(manga_path, 0755)
-	for _, url := range chapters {
-		download_chapter(url, name, manga_path)
+
+	chapterURLs := make(chan string)
+	max := make(chan struct{}, 3)
+	var wg sync.WaitGroup
+
+	go func() {
+		for chapterURL := range chapterURLs {
+			wg.Add(1)
+			max <- struct{}{}
+			go func(url string) {
+				defer func() {
+					<-max
+					wg.Done()
+				}()
+				downloadChapter(url, name, manga_path)
+			}(chapterURL)
+		}
+	}()
+
+	for _, chapterURL := range chapters {
+		chapterURLs <- chapterURL
 	}
+
+	//	for _, url := range chapters {
+	//		download_chapter(url, name, manga_path)
+	//	}
+	close(chapterURLs)
+	wg.Wait()
 
 }
 
-func download_chapter(chapter_url string, name string, manga_path string) {
-	resp, err := http.Get(chapter_url)
+func downloadChapter(chapterURL, name, mangaPath string) {
+	resp, err := http.Get(chapterURL)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 	defer resp.Body.Close()
 
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
+
 	var urls []string
 	doc.Find("div.container-chapter-reader").Find("img").Each(func(i int, s *goquery.Selection) {
 		src, _ := s.Attr("src")
 		urls = append(urls, src)
 	})
-	chapterSplit := strings.Split(chapter_url, "-")
+	chapterSplit := strings.Split(chapterURL, "-")
 	chapter := chapterSplit[len(chapterSplit)-1]
 
-	os.Mkdir(path.Join(manga_path, name+" "+chapter), 0755)
-	for index, chapter_url := range urls {
-		req, err := http.NewRequest("GET", chapter_url, nil)
-		if err != nil {
-			fmt.Println(err)
-		}
-		req.Header.Add("DNT", "1")
-		req.Header.Add("Referer", "https://readmanganato.com/")
-		req.Header.Add("sec-ch-ua-mobile", "?0")
-		req.Header.Add("sec-ch-ua-platform", "Linux")
-		req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36")
+	chapterDir := path.Join(mangaPath, name+" "+chapter)
+	os.Mkdir(chapterDir, 0755)
 
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-		}
+	imageSemaphore := make(chan struct{}, 5)
 
-		data, _ := ioutil.ReadAll(resp.Body)
-		ioutil.WriteFile(path.Join(manga_path, name+" "+chapter, strconv.Itoa(index)+".jpg"), data, 0600)
+	var wg sync.WaitGroup
+
+	for index, chapterURL := range urls {
+		imageSemaphore <- struct{}{}
+
+		wg.Add(1)
+		go func(index int, url, chapterDir string) {
+			defer func() {
+				<-imageSemaphore
+				wg.Done()
+			}()
+			downloadImage(index, url, chapterDir)
+		}(index, chapterURL, chapterDir)
 	}
+
+	wg.Wait()
 	fmt.Println("✅ " + name + " " + chapter)
+}
+
+func downloadImage(index int, imageURL, chapterDir string) {
+	req, err := http.NewRequest("GET", imageURL, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("DNT", "1")
+	req.Header.Add("Referer", "https://readmanganato.com/")
+	req.Header.Add("sec-ch-ua-mobile", "?0")
+	req.Header.Add("sec-ch-ua-platform", "Linux")
+	req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36")
+
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = ioutil.WriteFile(path.Join(chapterDir, strconv.Itoa(index)+".jpg"), data, 0600)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
