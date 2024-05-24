b8579f93053dcdb65a19f628dcd4c0b4378ae949
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2024-05-24 19:03:29 +0200

    Update sub_remover.py

--- Scripts/sub_remover.py
+++ Scripts/sub_remover.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import subprocess
 import os
+from threading import Thread
+from queue import Queue
 
 try:
     subprocess.run(["mkvmerge", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
@@ -11,17 +13,30 @@ except:
 
 path = os.getcwd()
 files = os.listdir(path)
-files =  [file for file in files if file.endswith(".mkv")]
-
+q = Queue()
+[q.put(file) for file in files if file.endswith(".mkv")]
+threads = min(25, q.qsize())
 options = {}
 
-for file in files:
-    mkvmerge_output = subprocess.run(["mkvmerge", "-i", os.path.join(path, file)], capture_output=True, text=True)
+def thread_job():
+    while not q.empty():
+        file = q.get()
+        mkvmerge_output = subprocess.run(["mkvmerge", "-i", os.path.join(path, file)], capture_output=True, text=True)
 
-    for line in mkvmerge_output.stdout.splitlines():
-        if "subtitles" in line and line not in options:
-            track_id = line.split()[2].strip(":")
-            options[track_id] = line
+        for line in mkvmerge_output.stdout.splitlines():
+            if "subtitles" in line and line not in options:
+                track_id = line.split()[2].strip(":")
+                options[track_id] = line
+        q.task_done()
+
+t_list = []
+for i in range(threads):
+    t = Thread(target=thread_job)
+    t_list.append(t)
+    t.start()
+
+for i in t_list:
+    i.join()
 
 print("\n".join(options.values()))
 
@@ -30,7 +45,17 @@ choice = input("\nWhat Track ID to use: ")
 while choice not in options:
     choice = input("\nSorry not a valid ID: ")
 
-for file in files:
-    mkvmerge_output = subprocess.run(["mkvmerge" , "-o", f"{os.path.join(path, file)}CACHE", "-s", choice, f"{os.path.join(path, file)}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    subprocess.run(["mv", f"{os.path.join(path, file)}CACHE", os.path.join(path, file)])  # Fixed this line
-    print(f"✅ {file} done")
+[q.put(file) for file in files if file.endswith(".mkv")]
+
+def main():
+    while not q.empty():
+        file = q.get()
+        mkvmerge_output = subprocess.run(["mkvmerge" , "-o", f"{os.path.join(path, file)}CACHE", "-s", choice, f"{os.path.join(path, file)}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(["mv", f"{os.path.join(path, file)}CACHE", os.path.join(path, file)])
+        print(f"✅ {file} done")
+
+t_list = []
+for i in range(threads):
+    t = Thread(target=main)
+    t_list.append(t)
+    t.start()
