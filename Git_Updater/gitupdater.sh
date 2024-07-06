ba75f1bb528c8f4cb3f43cba1852e6f5558d8dac
Tepz <tayyab2607@gmail.com>
2024-07-06 11:42:04 +0200

    Commit

--- Git_Updater/gitupdater.sh
+++ Git_Updater/gitupdater.sh
@@ -10,7 +10,7 @@ while read -r line; do
 		git add . > /dev/null 2>&1
 		git commit -m "Commit" > /dev/null 2>&1
 		git push > /dev/null 2>&1
-		echo ✅$line
+		echo ✅ $line
 	else
 		echo "Directory" $line not found
 	fi
