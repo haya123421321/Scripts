ae531c0116881f517b051da02f79953801792158
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2024-09-04 16:27:11 +0200

    Update nordea_calculate.py

--- Scripts/nordea_calculate.py
+++ Scripts/nordea_calculate.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from pdfminer.high_level import extract_text
 import os
@@ -20,12 +20,27 @@ def find_Nordea_files(directory):
 nordea_files = find_Nordea_files(sys.argv[1])
 total = 0
 
-for file in nordea_files:
+i = 0
+index = 3
+while True:
+    if i == len(nordea_files):
+        break
+    file = nordea_files[i]
     text = extract_text(file).split("\n")
-    index = text.index("Rentedato") + 4
-    kroner = text[index].strip("-DKK").strip().replace(",", ".")
-    total += float(kroner)
-    print(file + ":", str(round(float(kroner), 2)) + "kr")
+    try:
+        kroner_index = text.index("Rentedato") + index
+    except ValueError:
+        print("Something wrong with " + file)
+        i += 1
+        
+    try:
+        kroner = text[kroner_index].strip("-DKK").strip().replace(",", ".")
+        total += float(kroner)
+        print(file + ":", str(round(float(kroner), 2)) + "kr")
+        i += 1
+        index = 3
+    except:
+        index += 1
 
 
 print(f"Total: {round(total, 2)} kr")                                                 
