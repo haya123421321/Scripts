b7b7a3cdca3e18e18d6cfb4e47262070faf911ec
Tepz <tayyab2607@gmail.com>
2024-11-01 23:45:38 +0100

    Update

---
+++ server/Connect.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type json_info struct {
+	Username string `json:Username`
+	Default string `json:Default`
+}
+
+var data json_info
+var data_file_path string
+
+func main() {
+
+	user,err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	data_file_path = filepath.Join(user, "Server", "data.json")
+	
+	if _, err := os.Stat(data_file_path); err != nil {
+		os.MkdirAll(filepath.Dir(data_file_path), 0755)
+		os.Create(data_file_path)
+	}
+	
+	file,err := os.ReadFile(data_file_path)
+	if err != nil {
+		panic(err)
+	}
+
+	json.Unmarshal(file, &data)
+	
+	var ip string
+	if data.Default == "" {
+		fmt.Print("Enter the IP: ")
+		fmt.Scanln(&ip)
+	} else {
+		ip = data.Default
+	}
+	
+	var conn net.Conn
+	conn,err = net.Dial("tcp", ip + ":8080")
+	if err != nil {
+		for {
+			fmt.Print("Couldnt connect to "+ip+":8080 enter another ip: ")
+			fmt.Scanln(&ip)
+			conn,err = net.Dial("tcp", ip + ":8080")
+			if err == nil {
+				break
+			}
+
+		}
+	}
+
+	if data.Username != "" {
+		conn.Write([]byte("--USER:"+data.Username))
+	}
+	
+	go reader(conn)
+	go writer(conn)
+	
+	for {}
+}
+
+func reader(conn net.Conn) {
+	for {
+		buf := make([]byte, 1024)
+		_,err := conn.Read(buf)
+		if err != nil {
+			panic("%s closed connection\n" + conn.RemoteAddr().String())
+		}
+		fmt.Print(string(buf))
+	}
+}
+
+func writer(conn net.Conn) {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		Message, _ :=  reader.ReadString('\n')
+		Message = strings.TrimSpace(Message)
+
+		if Message == ":Default" {
+			data.Default = strings.Split(conn.RemoteAddr().String(), ":")[0]
+			s,_ := json.MarshalIndent(data, "", "\t")
+			os.WriteFile(data_file_path, s, 0755)
+			fmt.Print("Changed default to: " + strings.Split(conn.RemoteAddr().String(), ":")[0] + "\n")
+
+		} else if strings.Split(Message, " ")[0] == ":Username" {
+			if len(strings.Split(Message, " ")) > 1 {
+				username := strings.Split(Message, " ")[1]
+				data.Username = username
+				s,_ := json.MarshalIndent(data, "", "\t")
+				os.WriteFile(data_file_path, s, 0755)
+				fmt.Print("Changed username to: " + username + "\n")
+			} else {
+				fmt.Print("Usage: :Username <new-name>\n")
+			}
+		}
+
+		conn.Write([]byte(Message))
+	}
+}
---
+++ server/Host.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+//var (
+//	connections = make(map[string]net.Conn)
+//)
+
+type connection_data struct {
+	Name string
+	Connection net.Conn
+}
+
+var (
+	Connections = make(map[string]connection_data)
+)
+
+func main() {
+	listener,err := net.Listen("tcp", "0.0.0.0:8080")
+	if err != nil{
+		panic(err)
+	}
+	
+	fmt.Println("Started service on port 8080")
+
+	for {
+		conn,err := listener.Accept()
+		if err != nil {
+			return
+		}
+
+		buf := make([]byte, 1024)
+		conn.Read(buf)
+		
+		var username string
+		user_name_split := strings.Split(string(buf), ":")
+		if user_name_split[0] == "--USER" {
+			username = strings.Join(user_name_split[1:], ":")
+		} else {
+			username = ""
+		}
+
+		ip := strings.Split(conn.RemoteAddr().String(), ":")[0]
+
+		Connections[ip] = connection_data{
+			Name: username,
+			Connection: conn,
+		}
+		fmt.Printf("%s Joined\n", conn.RemoteAddr())
+
+		go handle(conn, ip)
+	}
+}
+
+func handle(conn net.Conn, ip string) {
+	user := Connections[ip].Name
+	conn.Write([]byte(user + ": "))
+	defer conn.Close()
+	for {
+		buf := make([]byte, 1024)
+		n,err := conn.Read(buf)
+
+		if err != nil {
+			fmt.Printf("%s closed connection\n", conn.RemoteAddr())
+			delete(Connections, strings.Split(conn.RemoteAddr().String(), ":")[0])
+			break
+		}
+		message := strings.TrimSpace(string(buf)[:n])
+
+		if message[0] == ':' {
+			if message == ":Help" {
+				conn.Write([]byte(":Default - Make this connection the default\n"))
+				conn.Write([]byte(":Username <new-name> - Change your username\n"))
+			}	
+			conn.Write([]byte(user + ": "))
+			continue
+		}
+
+		conn.Write([]byte(user + ": "))
+		for ip,connection := range Connections {
+			if ip == strings.Split(conn.RemoteAddr().String(), ":")[0]{
+				continue
+			}
+
+			go func(ip string) {
+				//connection.Write([]byte("\033[0G\033[2K" + conn.RemoteAddr().String() + ": " + message + "\n[USERNAME]: "))
+				connection.Connection.Write([]byte("\033[0G\033[2K" + connection.Name + ": " + message + "\n" + user + ": "))
+			}(ip)
+		}
+	}
+}
